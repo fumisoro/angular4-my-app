fe2a19cbcb362dae130aa5240e697803abf0d79a
fumisoro <fumipumisecky@gmail.com>
2017-06-20 23:14:58 +0900

    list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    console.log(hero);
+    this.selectedHero = hero;
   };
+
+  constructor(http: Http){
+    http.get('http://damrey.com:3000/api/archives/34441.json')
+    .map(res => res.json())
+    .subscribe(hoge => {
+      console.log(hoge);
+      alert("hoge");
+    });
+  };
+
 }
 
 export class Hero {
